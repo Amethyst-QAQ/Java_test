56fdbf5ed5eb059f87f9a547b9b95a5425d57e06
刘子骜 <liu-za20@mails.tsinghua.edu.cn>
2020-10-26 23:13:31 +0800

    Default Changelist

---
+++ src/xyz/chlamydomonos/Main.java
@@ -0,0 +1,9 @@
+package xyz.chlamydomonos;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Circle c1 = new ColoredCircle(1, 1);
+        ColoredCircle c2 = new ColoredCircle(1, 1);
+    }
+}
